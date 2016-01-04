3f68252d75fcef0d8d28b4f799a9043b16b420d9
hedejing <hedejin95@gmail.com>
2016-01-04 08:43:07 +0800

    Update main.cpp for multiple shadow

--- src/main.cpp
+++ src/main.cpp
@@ -54,9 +54,13 @@ void draw_world()
 	glRotatef(-45, 1, 0, 0);
 	glPopMatrix();
 }
+
+
 int main(int argc, char *argv[]) {
 	World::init(&argc, argv);
-	World::setCamera(Point(-10, 10, 10), Point(0, 0, 0));
+	//World::setCamera(Point(-10, 10, 10), Point(0, 0, 0));
+	World::setCamera(Point(12, 12, 12), Point(0, 1, 0));
+
 	//可以在这里使用glutIdleFunc();对idle进行重写
 	//若要对鼠标和键盘函数进行重写，建议在World.cpp里修改
 	
@@ -77,33 +81,35 @@ int main(int argc, char *argv[]) {
 	{  //LIGHTING
 		LightSource ls(Point(5, 6, -2, 1));
 		LightManager::addLight(ls);
-		//LightSource ls2(Point(-2, 6, 5, 1));
-		//LightManager::addLight(ls2);
-		//LightSource ls3(Point(-3, 6, -3, 1));
-		//LightManager::addLight(ls3);
-		//LightManager::lights.push_back(ls);
-		//float lightPos0[] = {1, 0, 0, 1};
+		LightSource ls2(Point(-2, 6, 5, 1));
+		LightManager::addLight(ls2);
+		LightSource ls3(Point(-3, 6, -3, 1));
+		LightManager::addLight(ls3);
 		//glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
-		glLightfv(GL_LIGHT0, GL_AMBIENT, Color(0, 0, 0, 0.5));
-		glLightfv(GL_LIGHT0, GL_DIFFUSE, COLOR::white);
-		glLightfv(GL_LIGHT0, GL_SPECULAR,COLOR::white);
-		//glLightfv(GL_LIGHT0, GL_AMBIENT, Color(1, 0, 0, 0.5));//COLOR::white);
-		//glLightfv(GL_LIGHT0, GL_DIFFUSE, Color(1, 0, 0, 0.5));//COLOR::white);
-		//glLightfv(GL_LIGHT0, GL_SPECULAR, Color(1, 0, 0, 0.5));//COLOR::white);
+		//glLightfv(GL_LIGHT0, GL_AMBIENT, Color(0, 0, 0, 0));
+		//glLightfv(GL_LIGHT0, GL_DIFFUSE, COLOR::white);
+		//glLightfv(GL_LIGHT0, GL_SPECULAR,COLOR::white);
+		glLightfv(GL_LIGHT0, GL_AMBIENT, Color(0, 0, 0, 0));//COLOR::white);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, Color(1,  0.2, 0.2, 0));//COLOR::white);
+		glLightfv(GL_LIGHT0, GL_SPECULAR, Color(1, 0.2, 0.2, 0));//COLOR::white);
+		//glLightfv(GL_LIGHT0, GL_DIFFUSE, Color(1, 1, 1, 0));//COLOR::white);
+		//glLightfv(GL_LIGHT0, GL_SPECULAR, Color(1,1, 1, 0));//COLOR::white);
 
-		//glLightfv(GL_LIGHT1, GL_AMBIENT, Color(0, 0, 0, 0.5));//COLOR::white);
-		//glLightfv(GL_LIGHT1, GL_DIFFUSE, Color(0, 0, 1, 0.5));//COLOR::white);
-		//glLightfv(GL_LIGHT1, GL_SPECULAR, Color(0, 0, 1, 0.5));//COLOR::white);
+		glLightfv(GL_LIGHT1, GL_AMBIENT, Color(0, 0, 0, 0));//COLOR::white);
+		glLightfv(GL_LIGHT1, GL_DIFFUSE, Color( 0.2, 0.2, 1, 0));//COLOR::white);
+		glLightfv(GL_LIGHT1, GL_SPECULAR, Color(0.2, 0.2, 1, 0));//COLOR::white);
+		//glLightfv(GL_LIGHT1, GL_DIFFUSE, Color( 1, 1, 1, 0));//COLOR::white);
+		//glLightfv(GL_LIGHT1, GL_SPECULAR, Color(1, 1, 1, 0));//COLOR::white);
 
-		//glLightfv(GL_LIGHT2, GL_AMBIENT, Color(0, 0, 0, 0.5));//COLOR::white);
-		//glLightfv(GL_LIGHT2, GL_DIFFUSE, Color(0, 1, 0, 0.5));//COLOR::white);
-		//glLightfv(GL_LIGHT2, GL_SPECULAR, Color(0, 1, 0, 0.5));//COLOR::white);
+		glLightfv(GL_LIGHT2, GL_AMBIENT, Color(0, 0, 0, 0));//COLOR::white);
+		glLightfv(GL_LIGHT2, GL_DIFFUSE, Color( 0.2, 1, 0.2, 0));//COLOR::white);
+		glLightfv(GL_LIGHT2, GL_SPECULAR, Color(0.2, 1, 0.2, 0));//COLOR::white);
 
 		/*GLfloat dir[] = { 0, 0,-1};
 		glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, dir);*/
-		glEnable(GL_LIGHT0);  //light0只是用做示例
-		glEnable(GL_LIGHT1);
-		glEnable(GL_LIGHT2);
+		//glEnable(GL_LIGHT0);  //light0只是用做示例
+		//glEnable(GL_LIGHT1);
+		//glEnable(GL_LIGHT2);
 	}
 
 	glutMainLoop();
